6f6e7c228a35bef74fe4f651f4a0812368e72e8e
Tihomir <tihomir.todorov@gmail.com>
2021-09-02 19:57:54 +0300

    animate logo with scroll event

--- src/scripts/bundle.js
+++ src/scripts/bundle.js
@@ -8,3 +8,43 @@ animation.classList.remove("is-animated");
 
 
 setTimeout(removeAnimation, 7000);
+
+
+/*
+document.addEventListener('scroll', function(e) {
+let heightContainer = document.querySelector(".heightContainer");
+
+	let welcomeSection = document.querySelector(".section-welcome");
+	let a = `scrollTop: ${welcomeSection.scrollTop}`;
+
+});
+*/
+window.addEventListener( 'load', function() {
+	logoZoomInZoomOut();
+
+  window.addEventListener( 'scroll', function() {
+        // normalize scroll position as percentage
+				logoZoomInZoomOut();
+
+
+  }, false);
+  
+}, false);
+
+function logoZoomInZoomOut() {
+	var box = document.querySelector(".overlay"),
+	docHeight = document.documentElement.offsetHeight;
+	var scrolled = (window.scrollY / ( docHeight - window.innerHeight )) * 30.2 + 1,
+	transformValue = 'scale('+Math.ceil(scrolled * 100) / 100+')';
+
+	console.log(scrolled);
+	if(scrolled > 28) {
+		box.style.display = "none";
+	}else {
+		box.style.WebkitTransform = transformValue;
+		box.style.MozTransform = transformValue;
+		box.style.OTransform = transformValue;
+		box.style.transform = transformValue;
+		box.style.display = "block";
+}
+}
